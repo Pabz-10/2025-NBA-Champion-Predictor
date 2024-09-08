93c101444b2e5219dc3f43c46d06c7bf7c65a405
Pabil <pabil.adhikari10@gmail.com>
2024-09-07 19:43:17 -0700

    updated data

--- data.py
+++ data.py
@@ -8,12 +8,15 @@ clutch_stats = leaguedashteamclutch.LeagueDashTeamClutch(
     season_type_all_star="Regular Season",
     clutch_time="Last 5 Minutes",
     ahead_behind="Ahead or Behind",
-    point_diff="5"
+    point_diff="5"  
 )
 
-# convert data to a data frame
+# create a df for the stats and save as csv file
 clutch_stats_df = clutch_stats.get_data_frames()[0]
 clutch_stats_df.to_csv('clutch_stats.csv', index=False)
+
+nba_d = pd.read_csv('clutch_stats.csv')
+print(nba_d.head())
 
 
 
@@ -26,7 +29,7 @@ clutch_stats_df.to_csv('clutch_stats.csv', index=False)
 Algo for determining the champion will take 4 catagories into account
 1. Effective field goal EFg = ((2*FGM +1.5*3PM)/FGA): 40%
 2. Defensive rating: 30%
-3. To % lower the better: 20%
-3. +/-: 10%
+3. To % lower the better: 10%
+3. defensive rebounds: 20%
 '''
 

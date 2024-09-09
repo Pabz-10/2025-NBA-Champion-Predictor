e4385708c2550ca03ff295e8255aadb58ca14f78
Pabil <pabil.adhikari10@gmail.com>
2024-09-08 19:54:49 -0700

    updated algo

--- data.py
+++ data.py
@@ -12,8 +12,8 @@ clutch_stats = leaguedashteamclutch.LeagueDashTeamClutch(
 )
 
 #create a df for the stats and save as csv file
-clutch_stats_df = clutch_stats.get_data_frames()[0]
-clutch_stats_df.to_csv('clutch_stats.csv', index=False)
+# clutch_stats_df = clutch_stats.get_data_frames()[0]
+# clutch_stats_df.to_csv('clutch_stats.csv', index=False)
 nba_data = pd.read_csv('clutch_stats.csv')
 nba_data.head()
 
@@ -21,26 +21,26 @@ nba_data.head()
 Algo for determining the champion will take 4 catagories into account
 1. Effective field goal eFG = ((2*FGM +1.5*3PM)/FGA): 40%
 2. Defensive rating: 30%
-3. defensive rebounds: 20%
+3. Total rebounds: 20%
 4. TO% lower the better: 10%
 '''
 # Grab our values 
 nba_data["eFG"] = ((2 *nba_data["FGM"] + 1.5 * nba_data["FG3M"]) / nba_data["FGA"])
 nba_data["Def_Rtg"] = nba_data["DEF_RTG"]
-nba_data["DREB"] = nba_data["DREB"]
+nba_data["REB"] = nba_data["REB"]
 nba_data['TO%'] = nba_data['TOV']
 
 #Get our decile values
 nba_data["eFG_decile"] = pd.qcut(nba_data["eFG"], 10, labels=False, duplicates="drop") + 1
 nba_data["Def_Rtg_decile"] = pd.qcut(nba_data["Def_Rtg"], 10, labels=False, duplicates="drop") + 1
-nba_data["DREB_decile"] = pd.qcut(nba_data["DREB"], 10, labels=False, duplicates="drop") + 1
+nba_data["REB_decile"] = pd.qcut(nba_data["REB"], 10, labels=False, duplicates="drop") + 1
 nba_data["TO%_decile"] = pd.qcut(nba_data["TO%"], 10, labels=False, duplicates="drop") + 1
 
 #Pop these values into my algo
 nba_data["Finals"] = (
     (0.4 * nba_data["eFG_decile"]) +
     (0.3 * nba_data["Def_Rtg_decile"]) +
-    (0.2 * nba_data["DREB_decile"]) -
+    (0.2 * nba_data["REB_decile"]) -
     (0.1 * nba_data['TO%_decile'])
     )
 
